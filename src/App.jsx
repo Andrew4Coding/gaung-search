67187ac3746f0346f124b5dca544c49badc33c62
Andrew4Coding <andrewdevitoaryo@gmail.com>
2023-10-23 18:42:01 +0700

    filter upgraded

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import {data} from './assets/datamahasiswa'
+import { dataKelas } from './assets/datakelas'
 import Logo from './assets/Logo1.png'
 
+let x = ["", "", "", "", "", ""]
 function App() {
+  const [value, setValue] = useState('')
   const [search, setSearch] = useState("");
+
   return (
     <>
       <div className='container'>
@@ -15,6 +19,40 @@ function App() {
           <input type="text" placeholder="Search Something" id="search-field" 
           onChange={(e) => setSearch(e.target.value)}/>
         </div>
+        
+        <div className='filter-container'>
+      {dataKelas.map(kelas => (
+        <>
+          {kelas.name}
+          <select name="select-kelas" id={"filter-button"} key={kelas.id} onChange={(e) => {
+            const selectedOption = e.target.value;
+            setSearch(e.target.value)
+            if (kelas.name == "DDP1"){
+              x[0] = selectedOption
+            }
+            else if (kelas.name == "Calc1"){
+              x[1] = selectedOption
+            }
+            else if (kelas.name == "MD1"){
+              x[2] = selectedOption
+            }
+            else if (kelas.name == "PSD"){
+              x[3] = selectedOption 
+            }
+            else if (kelas.name == "Manbis"){
+              x[4] = selectedOption 
+            }
+            else if (kelas.name == "Kombis"){
+              x[5] = selectedOption
+            }
+          }}>
+            {kelas.value.map(option => (
+              <option value={option}>{option} </option>
+            ))}
+          </select>
+        </>
+      ))}
+    </div>
       </div>
       <div className='row' id='row-head'>
         <table>
@@ -33,7 +71,11 @@ function App() {
       </div>
 
       {
-        data.filter((item) => item.Nama.toLowerCase().includes(search.toLowerCase()) || String(item.NPM).includes(search))
+        data.filter((item) => (item.Nama.toLowerCase().includes(search.toLowerCase()) || String(item.NPM).includes(search)) 
+        && (item['DDP 1'].includes(x[0]) && item['Kalkulus 1'].includes(x[1]) && item['MD 1'].includes(x[2]) && item['PSD'].includes(x[3]) 
+        && item['Manbis'].includes(x[4]) && item['Kombistek'].includes(x[5]))
+        
+        )
         .map((item) => {
           return (
           <div className='row' key={item.No}>
@@ -54,6 +96,10 @@ function App() {
           )
         })
       }
+
+      <div className='question'>
+        <button>Dev?</button>
+      </div>
     </>
   )
 }
---
+++ src/assets/datakelas.js
@@ -0,0 +1,32 @@
+export const dataKelas = [
+    {
+        id: 1,
+        "name": "DDP1",
+        "value": ["", "A", "B", "C", "D"]
+    },
+    {
+        id: 2,
+        "name": "Calc1",
+        "value": ["", "A", "B", "C", "D"]
+    },
+    {
+        id: 3,
+        "name": "MD1",
+        "value": ["", "A", "B", "C", "D"]
+    },
+    {
+        id: 4,
+        "name": "PSD",
+        "value": ["", "A", "B", "C", "D"]
+    },
+    {
+        id: 5,
+        "name": "Manbis",
+        "value": ["", "A", "B", "C", "D"]
+    },
+    {
+        id: 6,
+        "name": "Kombis",
+        "value": ["", "A", "B", "C", "D"]
+    }
+]

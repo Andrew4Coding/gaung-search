f8c77112c2ad32845ca8ecfca7692249a88ad013
Andrew4Coding <andrewdevitoaryo@gmail.com>
2023-10-25 08:45:15 +0700

    fixed database

--- src/assets/datakelas.js
+++ src/assets/datakelas.js
@@ -2,27 +2,27 @@ export const dataKelas = [
     {
         id: 1,
         "name": "DDP1",
-        "value": ["", "A", "B", "C", "D"]
+        "value": ["", "A", "B", "C", "D", "E", "F", "G", "H"]
     },
     {
         id: 2,
         "name": "Calc1",
-        "value": ["", "A", "B", "C", "D"]
+        "value": ["", "A", "B", "C", "D", "E", "F", "G"]
     },
     {
         id: 3,
         "name": "MD1",
-        "value": ["", "A", "B", "C", "D"]
+        "value": ["", "A", "B", "C", "D", "E", "F", "G"]
     },
     {
         id: 4,
         "name": "PSD",
-        "value": ["", "A", "B", "C", "D"]
+        "value": ["", "A", "B", "C"]
     },
     {
         id: 5,
         "name": "Manbis",
-        "value": ["", "A", "B", "C", "D"]
+        "value": ["", "A", "B", "C"]
     },
     {
         id: 6,
